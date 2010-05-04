59334a391ba2004725c0d0f3984b364fe660089c
julien.delange <julien.delange@129961e7-ef38-4bb5-a8f7-c9a525a55882>
2010-05-04 12:15:45 +0000

    * integrate drivers, case study rasta compiles
    git-svn-id: https://tecsw.estec.esa.int/svn/taste/trunk/po-hi-c@537 129961e7-ef38-4bb5-a8f7-c9a525a55882

---
+++ include/drivers/po_hi_driver_rasta_serial.h
@@ -0,0 +1,15 @@
+/*
+ * This is a part of PolyORB-HI-C distribution, a minimal
+ * middleware written for generated code from AADL models.
+ * You should use it with the Ocarina toolsuite.
+ *
+ * Copyright (C) 2010, European Space Agency
+ */
+
+#ifndef __PO_HI_DRIVER_RASTA_SERIAL_H__
+#define __PO_HI_DRIVER_RASTA_SERIAL_H__
+
+void __po_hi_c_driver_serial_rasta_poller (void);
+
+#endif
+
