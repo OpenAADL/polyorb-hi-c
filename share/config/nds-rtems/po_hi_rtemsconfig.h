275494afd3487977dc337a4d0955ed4049183d9f
julien.delange <julien.delange@129961e7-ef38-4bb5-a8f7-c9a525a55882>
2010-06-01 21:50:35 +0000

    * add nds.rtems BSP
    git-svn-id: https://tecsw.estec.esa.int/svn/taste/trunk/po-hi-c@721 129961e7-ef38-4bb5-a8f7-c9a525a55882

---
+++ share/config/nds-rtems/po_hi_rtemsconfig.h
@@ -0,0 +1,14 @@
+/*
+ * This is a part of PolyORB-HI-C distribution, a minimal
+ * middleware written for generated code from AADL models.
+ * You should use it with the Ocarina toolsuite.
+ *
+ * For more informations, please visit http://ocarina.enst.fr
+ *
+ * Copyright (C) 2010, European Space Agency.
+ */
+
+#include <deployment.h>
+
+#define CONFIGURE_LIBIO_MAXIMUM_FILE_DESCRIPTORS 32
+
