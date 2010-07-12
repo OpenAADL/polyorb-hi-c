67ad4ecc283ced67515ce185c918a544d0b090c4
julien.delange <julien.delange@129961e7-ef38-4bb5-a8f7-c9a525a55882>
2010-07-12 18:05:54 +0000

    import new exarm drivers
    git-svn-id: https://tecsw.estec.esa.int/svn/taste/trunk/po-hi-c@990 129961e7-ef38-4bb5-a8f7-c9a525a55882

---
+++ src/drivers/po_hi_driver_exarm_ni_6071e_digital.c
@@ -0,0 +1,21 @@
+#include <deployment.h>
+
+#ifdef __PO_HI_NEED_DRIVER_EXARM_NI_6071E_DIGITAL
+
+#include <po_hi_types.h>
+
+void __po_hi_c_driver_exarm_ni_6071e_digital_init (__po_hi_device_id device_id)
+{
+   return;
+}
+ 
+
+void __po_hi_c_driver_exarm_ni_6071e_digital_poller (__po_hi_uint8_t* data1, __po_hi_uint8_t* data2)
+{
+   *data1 = 3;
+   *data2 = 4;
+   return;
+}
+
+#endif
+
