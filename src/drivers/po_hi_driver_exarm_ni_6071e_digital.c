40742dbf8c868ea403eb6a8cbcbea1c50fe17c9d
julien.delange <julien.delange@129961e7-ef38-4bb5-a8f7-c9a525a55882>
2010-07-13 08:14:29 +0000

    finalize ni6071e driver
    git-svn-id: https://tecsw.estec.esa.int/svn/taste/trunk/po-hi-c@991 129961e7-ef38-4bb5-a8f7-c9a525a55882

--- src/drivers/po_hi_driver_exarm_ni_6071e_digital.c
+++ src/drivers/po_hi_driver_exarm_ni_6071e_digital.c
@@ -2,18 +2,41 @@
 
 #ifdef __PO_HI_NEED_DRIVER_EXARM_NI_6071E_DIGITAL
 
+#include <comedilib.h>
 #include <po_hi_types.h>
+#include <po_hi_debug.h>
+
+comedi_t *po_hi_driver_exarm_ni_6071e_digital_it;
 
 void __po_hi_c_driver_exarm_ni_6071e_digital_init (__po_hi_device_id device_id)
 {
+   int ret;
+
+   po_hi_driver_exarm_ni_6071e_digital_it = comedi_open("/dev/comedi0");
+
+   ret = comedi_dio_config(po_hi_driver_exarm_ni_6071e_digital_it, 2, 1, COMEDI_INPUT);
+
+   if (ret == -1)
+   {
+      __DEBUGMSG ("Error when invoking comedi_dio_config()");
+   }
+
+   ret = comedi_dio_config(po_hi_driver_exarm_ni_6071e_digital_it, 2, 0, COMEDI_INPUT);
+
+   if (ret == -1)
+   {
+      __DEBUGMSG ("Error when invoking comedi_dio_config()");
+   }
+
    return;
 }
  
 
 void __po_hi_c_driver_exarm_ni_6071e_digital_poller (__po_hi_uint8_t* data1, __po_hi_uint8_t* data2)
 {
-   *data1 = 3;
-   *data2 = 4;
+
+   comedi_dio_read(po_hi_driver_exarm_ni_6071e_digital_it, 2, 1, (unsigned int*)data1);
+   comedi_dio_read(po_hi_driver_exarm_ni_6071e_digital_it, 2, 0, (unsigned int*)data2);
    return;
 }
 
