a9069ae9cb76c527c412ee67fbef1a9fd42927a4
julien.delange <julien.delange@129961e7-ef38-4bb5-a8f7-c9a525a55882>
2010-07-14 09:39:19 +0000

    finalize exarm driver, tested and validated on real hardware
    git-svn-id: https://tecsw.estec.esa.int/svn/taste/trunk/po-hi-c@1004 129961e7-ef38-4bb5-a8f7-c9a525a55882

--- include/drivers/po_hi_driver_exarm_ni_6071e_digital.h
+++ include/drivers/po_hi_driver_exarm_ni_6071e_digital.h
@@ -7,7 +7,7 @@
 void __po_hi_c_driver_exarm_ni_6071e_digital_init (__po_hi_device_id device_id);
  
 
-void __po_hi_c_driver_exarm_ni_6071e_digital_poller (__po_hi_uint8_t* data1, __po_hi_uint8_t* data2);
+void __po_hi_c_driver_exarm_ni_6071e_digital_poller (unsigned int* data1, unsigned int* data2);
 
 #endif
 
--- src/drivers/po_hi_driver_exarm_ni_6071e_digital.c
+++ src/drivers/po_hi_driver_exarm_ni_6071e_digital.c
@@ -32,9 +32,8 @@ void __po_hi_c_driver_exarm_ni_6071e_digital_init (__po_hi_device_id device_id)
 }
  
 
-void __po_hi_c_driver_exarm_ni_6071e_digital_poller (__po_hi_uint8_t* data1, __po_hi_uint8_t* data2)
+void __po_hi_c_driver_exarm_ni_6071e_digital_poller (unsigned int* data1, unsigned int* data2)
 {
-
    comedi_dio_read(po_hi_driver_exarm_ni_6071e_digital_it, 2, 1, (unsigned int*)data1);
    comedi_dio_read(po_hi_driver_exarm_ni_6071e_digital_it, 2, 0, (unsigned int*)data2);
    return;
