b3e122eb35d89eeca9aa062e498c9b5f343c9db0
julien.delange <julien.delange@129961e7-ef38-4bb5-a8f7-c9a525a55882>
2010-08-04 16:56:08 +0000

    integrate xc4v driver in pohic
    git-svn-id: https://tecsw.estec.esa.int/svn/taste/trunk/po-hi-c@1221 129961e7-ef38-4bb5-a8f7-c9a525a55882

---
+++ include/drivers/po_hi_driver_xc4v_fpga.h
@@ -0,0 +1,22 @@
+#include <deployment.h>
+
+#ifdef __PO_HI_NEED_DRIVER_XC4V_FPGA
+
+int __po_hi_driver_xc4v_fpga_initialize ();
+/*
+ * Return 1 if success, 0 otherwise
+ */
+
+unsigned __po_hi_driver_xc4v_fpga_read_register (unsigned offset);
+/*
+ * Read a register in the FPGA at offset position.
+ * Returns the value of the register.
+ */
+
+void __po_hi_driver_xc4v_fpga_write_register (unsigned offset, unsigned value);
+/*
+ * Write the register at offset with the value value in parameters.
+ */
+
+#endif /* __PO_HI_NEED_DRIVER_XC4V_FPGA */
+
