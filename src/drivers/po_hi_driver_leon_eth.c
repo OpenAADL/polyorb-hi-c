c6c2553ad243e494e3d745fcb2e9b109fd85a21c
jdelange <jdelange@129961e7-ef38-4bb5-a8f7-c9a525a55882>
2011-11-09 17:48:53 +0000

    * po_hi_driver_leon_eth.c    - Make the LEON ethernet driver compliant with the latest task API
    git-svn-id: https://tecsw.estec.esa.int/svn/taste/trunk/po-hi-c@3426 129961e7-ef38-4bb5-a8f7-c9a525a55882

--- src/drivers/po_hi_driver_leon_eth.c
+++ src/drivers/po_hi_driver_leon_eth.c
@@ -378,7 +378,7 @@ void __po_hi_c_driver_eth_leon_init (__po_hi_device_id id)
       __po_hi_initialize_add_task ();
 
       __po_hi_create_generic_task 
-         (-1, 0,__PO_HI_MAX_PRIORITY, 0, (void* (*)(void)) __po_hi_c_driver_eth_leon_poller);
+         (-1, 0,__PO_HI_MAX_PRIORITY, 0, (void* (*)(void)) __po_hi_c_driver_eth_leon_poller, NULL);
    }
 
    /*
