c6e4bc1544d66c19925d76316ec6f0a342ac6073
Christophe Garion <tofgarion@runbox.com>
2014-01-17 11:25:36 +0100

    add more tests for swap_byte

--- tests-proof/test_swap_byte_trivial.c
+++ tests-proof/test_swap_byte_trivial.c
@@ -13,16 +13,51 @@ unsigned long filter_ff(unsigned long value) {
 }
 
 /*@
+  @ ensures \result == (value & 0x000000ff) >> 24;
+  @*/
+unsigned long filter_shift(unsigned long value) {
+    return (value & 0x000000ff) >> 24;
+}
+
+/*@
+  @ ensures \result == (((value & 0x000000ff) >> 24) | ((value & 0x0000ff00) >> 8));
+  @*/
+unsigned long partial_swap(unsigned long value) {
+    return ((value & 0x000000ff) >> 24) | ((value & 0x0000ff00) >> 8);
+}
+
+/*@
   @ ensures \result == ((value & 0x000000ff) << 24) +
   @                    ((value & 0x0000ff00) << 8)  +
   @                    ((value & 0x00ff0000) >> 8)  +
   @                    ((value & 0xff000000) >> 24);
   @*/
+unsigned long swap_shift_unvalid(unsigned long value) {
+    unsigned long v1 = (value & 0x000000ff) << 24;
+    unsigned long v2 = (value & 0x0000ff00) << 8;
+    unsigned long v3 = (value & 0x00ff0000) >> 8;
+    unsigned long v4 = (value & 0xff000000) >> 24;
+
+    unsigned long v = v1 + v2 + v3 + v4;
+
+    return v;
+}
+
+/*@
+  @ ensures \result == (((value & 0x000000ff) << 24) |
+  @                     ((value & 0x0000ff00) << 8)  |
+  @                     ((value & 0x00ff0000) >> 8)  |
+  @                     ((value & 0xff000000) >> 24));
+  @*/
 unsigned long swap_shift(unsigned long value) {
-    return ((value & 0x000000ff) << 24) +
-        ((value & 0x0000ff00) << 8)  +
-        ((value & 0x00ff0000) >> 8)  +
-        ((value & 0xff000000) >> 24);
+    unsigned long v1 = (value & 0x000000ff) << 24;
+    unsigned long v2 = (value & 0x0000ff00) << 8;
+    unsigned long v3 = (value & 0x00ff0000) >> 8;
+    unsigned long v4 = (value & 0xff000000) >> 24;
+
+    unsigned long v = v1 | v2 | v3 | v4;
+
+    return v;
 }
 
 /*@
