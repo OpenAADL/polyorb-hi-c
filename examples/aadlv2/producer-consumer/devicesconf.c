993357e9925fb5b3ea07fda446773319a576660c
yoogx <hugues.jerome@gmail.com>
2014-08-25 10:18:37 +0200

    * Adjustments for distcheck if no doc is built

--- examples/aadlv2/producer-consumer/devicesconf.c
+++ examples/aadlv2/producer-consumer/devicesconf.c
@@ -1,4 +1,4 @@
 #include <drivers/configuration/ip.h>
 
-__po_hi_c_ip_conf_t pohidrv_device_a = { "eth0", "127.0.0.1", __po_hi_c_ipv4, 1234};
-__po_hi_c_ip_conf_t pohidrv_device_b = { "eth0", "127.0.0.1", __po_hi_c_ipv4, 1235};
+__po_hi_c_ip_conf_t pohidrv_device_a = { "eth0", "127.0.0.1", "","","","",__po_hi_c_ipv4, 1234};
+__po_hi_c_ip_conf_t pohidrv_device_b = { "eth0", "127.0.0.1", "","","","",__po_hi_c_ipv4, 1235};
