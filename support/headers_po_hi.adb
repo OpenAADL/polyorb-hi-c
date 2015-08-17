aecf50631567003349fb148c1a918aa08f5e4c29
yoogx <hugues.jerome@gmail.com>
2015-08-17 22:14:54 +0200

    * Correct copyright year

--- support/headers_po_hi.adb
+++ support/headers_po_hi.adb
@@ -6,7 +6,7 @@
 --                                                                          --
 --                                 B o d y                                  --
 --                                                                          --
---    Copyright (C) 2006-2009 Telecom ParisTech, 2010-2012 ESA & ISAE.      --
+--    Copyright (C) 2006-2009 Telecom ParisTech, 2010-2015 ESA & ISAE.      --
 --                                                                          --
 -- PolyORB-HI is free software; you can redistribute it and/or modify under --
 -- terms of the  GNU General Public License as published  by the Free Soft- --
