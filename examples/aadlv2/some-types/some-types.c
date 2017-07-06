26f96c149cb20320d74a129ab5de7b6f2f6aebc4
yoogx <hugues.jerome@gmail.com>
2017-07-06 09:01:32 -0400

    * Extend some-types example to support arrays
            For openaadl/ocarina#99

--- examples/aadlv2/some-types/some-types.c
+++ examples/aadlv2/some-types/some-types.c
@@ -1,3 +1,14 @@
+/*
+ * This is a part of PolyORB-HI-C distribution, a minimal
+ * middleware written for generated code from AADL models.
+ * You should use it with the Ocarina toolsuite.
+ *
+ * For more informations, please visit http://taste.tuxfamily.org/wiki
+ *
+ * Copyright (C) 2017 ESA & ISAE.
+ */
+
+#include <assert.h>
 #include <stdio.h>
 #include <types.h>
 
@@ -23,11 +34,20 @@ void user_receive_boolean (int boolean)
   printf ("Receiving boolean : %d\n", boolean);
 }
 
-void user_emit_integer( int* integer)
+void user_emit_integer (int* integer)
 {
   integer_type_var++;
   *integer = integer_type_var;
   printf ("Emetting integer : %d\n", *integer);
+}
+
+void user_emit_array (software__array_type* data_source)
+{
+  for (int i = 0; i < 16384; i++)
+    (*data_source)[i]=i;
+
+  printf ("Emetting array\n");
+  printf ("\n");
 }
 
 void user_receive_integer (int integer)
@@ -42,7 +62,7 @@ void user_emit_struct (software__struct_type_impl *v)
 
   value.x = i;
   value.y = i;
-  
+
   printf ("*** SENDING PING *** %d\n", i);
   i++;
   *v = value;
@@ -53,4 +73,14 @@ void user_receive_struct (software__struct_type_impl i)
 {
   printf ("*** PING *** (%d, %d)\n" ,i.x, i.y);
   fflush (stdout);
+}
+
+void user_receive_array (software__array_type data)
+{
+  printf("Receive array: ");
+  for (int i = 0; i < 16; i++)
+    assert (data[i] == i);
+
+  printf (" OK \n");
+  fflush(stdout);
 }
