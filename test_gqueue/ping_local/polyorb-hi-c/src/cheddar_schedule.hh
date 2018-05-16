b5487301b1d2eb69bde2e104c5be95ffb4163766
Antonia Francis <afrancis.supaero@gmail.com>
2018-05-16 16:55:04 +0200

    add of fseek

--- test_gqueue/ping_local/polyorb-hi-c/src/cheddar_schedule.hh
+++
@@ -1,8 +0,0 @@
-extern "C"{
-void    add_edge(int vertex_id_1,int vertex_id_2);
-void    add_vertex(int vertex_id, int task, int local_step, int global_step);
-void    set_hyperperiod(unsigned int nb_steps);
-void    print_graph_debug();
-void    serialize_graph();
-void    unserialize_graph();
-}
