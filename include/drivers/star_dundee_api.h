0bd10713153549ac6e8d4bdd87d46927e72d6f05
yoogx <hugues.jerome@gmail.com>
2018-07-31 22:58:42 +0200

    * Wrapper API for Star Dundee Mk3 SpaceWire devices
            For openaadl/ocarina#170

---
+++ include/drivers/star_dundee_api.h
@@ -0,0 +1,30 @@
+/*
+ * This is a part of PolyORB-HI-C distribution, a minimal
+ * middleware written for generated code from AADL models.
+ * You should use it with the Ocarina toolsuite.
+ *
+ * For more informations, please visit http://taste.tuxfamily.org/wiki
+ *
+ * Copyright (C) 2018 ESA & ISAE.
+ */
+
+#ifndef __STAR_DUNDEE_API_H__
+#define __STAR_DUNDEE_API_H__
+
+/* Wrapper for Star Dundee Mk3 API */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <stddef.h>
+#include "star-api.h"
+#include "star-dundee_types.h"
+
+/** Send message through selectedChannel */
+size_t dundee_sending(void* message, int message_size, STAR_CHANNEL_ID selectedChannel);
+
+/** Receive message through selectedChannel */
+size_t dundee_receiving(void* message, STAR_CHANNEL_ID selectedChannel);
+
+#endif /*__STAR_DUNDEE_API_H__ */
---
+++ src/drivers/star_dundee_api.c
@@ -0,0 +1,137 @@
+/*
+ * This is a part of PolyORB-HI-C distribution, a minimal
+ * middleware written for generated code from AADL models.
+ * You should use it with the Ocarina toolsuite.
+ *
+ * For more informations, please visit http://taste.tuxfamily.org/wiki
+ *
+ * Copyright (C) 2018 ESA & ISAE.
+ */
+
+#include<po_hi_debug.h>
+#include "star_dundee_api.h"
+
+/******************************************************************************/
+size_t dundee_sending
+(void* message,
+ int message_size,
+ STAR_CHANNEL_ID selectedChannel) {
+
+  size_t size = message_size;
+  if (size ==0){
+    __PO_HI_DEBUG_CRITICAL
+      ("The length of the to-be-sent packet is 0, aborting.\n");
+    return 0;
+  }
+
+  /* Create a message_size packet to be transmitted, terminated with an EOP */
+  STAR_STREAM_ITEM *pTxStreamItem;
+  STAR_SPACEWIRE_ADDRESS *pAddress = NULL;
+
+  /* Note: for the moment, message_size is only the size of the data
+     (without header) */
+
+  pTxStreamItem = STAR_createPacket
+    (pAddress, message, message_size, STAR_EOP_TYPE_EOP);
+  if (!pTxStreamItem){
+    __PO_HI_DEBUG_CRITICAL
+      ("Couldn't create the stream item to be transmitted.\n");
+    return 0;
+  }
+
+  /* Create a transfer operation to transmit the packet */
+
+  STAR_TRANSFER_OPERATION *pTxTransOp;
+  pTxTransOp = STAR_createTxOperation(&pTxStreamItem, 1);
+  if (!pTxTransOp){
+    __PO_HI_DEBUG_CRITICAL("Couldn't create the transmit operation.\n");
+    STAR_destroyStreamItem(pTxStreamItem);
+    return 0;
+  }
+
+  /* Start transmitting the packet */
+  (void)STAR_submitTransferOperation(selectedChannel, pTxTransOp);
+
+  /* Wait indefinitely on the transmit completing */
+  STAR_TRANSFER_STATUS status;
+  status = STAR_waitOnTransferOperationCompletion(pTxTransOp, -1);
+  if (status != STAR_TRANSFER_STATUS_COMPLETE){
+    __PO_HI_DEBUG_CRITICAL("Could not transmit packet, error of %d.", status);
+    return 0;
+  }
+
+  /* Dispose of the transmit operation */
+  (void)STAR_disposeTransferOperation(pTxTransOp);
+
+  /* Destroy the transmit packet */
+  STAR_destroyStreamItem(pTxStreamItem);
+
+  /* Return the size of the data */
+  return size;
+}
+
+/******************************************************************************/
+size_t dundee_receiving(void* message, STAR_CHANNEL_ID selectedChannel){
+
+  size_t size = 0;
+  unsigned int receiveBufferLength;
+  unsigned char *pReceiveBuffer;
+
+  /* Create a transfer operation for receiving one packet */
+  STAR_TRANSFER_OPERATION *pRxTransOp;
+  pRxTransOp = STAR_createRxOperation(1, STAR_RECEIVE_PACKETS);
+  if (!pRxTransOp){
+    __PO_HI_DEBUG_CRITICAL("Couldn't create receive operation, exiting.\n");
+    return 0;
+  }
+
+  /* Start receiving a packet */
+  STAR_submitTransferOperation(selectedChannel, pRxTransOp);
+
+  /* Wait indefinitely on a packet being received */
+  STAR_TRANSFER_STATUS status;
+  status = STAR_waitOnTransferOperationCompletion(pRxTransOp, -1);
+  if (status != STAR_TRANSFER_STATUS_COMPLETE){
+    __PO_HI_DEBUG_CRITICAL("Could not receive packet, error of %d.\n", status);
+    STAR_disposeTransferOperation(pRxTransOp);
+    return 0;
+  }
+
+  /* Get the stream item received */
+  STAR_STREAM_ITEM *pRxStreamItem;
+  pRxStreamItem = STAR_getTransferItem(pRxTransOp, 0);
+  if (!pRxStreamItem){
+    __PO_HI_DEBUG_CRITICAL("Unable to get the stream item received. \n");
+    STAR_disposeTransferOperation(pRxTransOp);
+    return 0;
+  }
+
+  /* Get the bytes of the packet received */
+  pReceiveBuffer = STAR_getPacketData((STAR_SPACEWIRE_PACKET *)pRxStreamItem->item,&receiveBufferLength);
+  if (!pReceiveBuffer){
+    __PO_HI_DEBUG_CRITICAL("Unable to get the contents of the packet.\n");
+    STAR_disposeTransferOperation(pRxTransOp);
+    return 0;
+  }
+
+  /* Here the length is both header and data */
+
+  memcpy (message, pReceiveBuffer, receiveBufferLength);
+
+  /* Display the bytes in the packet */
+  __PO_HI_DEBUG_CRITICAL("Received packet contents:\n");
+  __PO_HI_DEBUG_CRITICAL(" \n");
+  size = receiveBufferLength;
+
+  if (size ==0){
+    __PO_HI_DEBUG_CRITICAL("The length of the received packet is 0. \n");
+  }
+
+  /* Destroy the buffer containing the received packet, once the
+     message is copied. */
+  STAR_destroyPacketData(pReceiveBuffer);
+
+  /* Dispose of the receive operation */;
+  STAR_disposeTransferOperation(pRxTransOp);
+  return size;
+}
