adf08ce5ad18bb53043b9071a7eeb4db0313b7a6
yoogx <hugues.jerome@gmail.com>
2018-09-07 14:52:52 +0200

    * Add StarDundee API iff using PRISE, do not build the SpW           example by default

--- examples/aadlv2/producer-consumer/userdefined.mk
+++ examples/aadlv2/producer-consumer/userdefined.mk
@@ -1,2 +1,11 @@
-USER_CFLAGS= -I/opt/STAR-System/inc/star
+# The following is specific to ISAE setting, to use SpaceWire
+# API. Adapt to your own setting
+
+HOSTNAME:= $(shell hostname)
+
+ifeq ($(HOSTNAME),prise-space-7)
+ifeq ($(TARGET),native)
+USER_CFLAGS= -I/opt/prise/STAR-Dundee/inc/star
 USER_LDFLAGS= -lstar-api -lstar_conf_api_router -lstar_conf_api_mk2
+endif
+endif
